70f0e589915d77ee830e46ec68e179bd38de27b4
Knaro <84656846+Knaro009@users.noreply.github.com>
2022-05-08 01:34:48 -0700

    Create call-all.lua

---
+++ call-all.lua
@@ -0,0 +1,13 @@
+--my fren made this but i will put in this rep
+local StarterGui = game:GetService("StarterGui")
+for i,v in pairs(game.Players:GetChildren()) do
+    if v.Name ~= game.Players.LocalPlayer.Name then
+        game.Players.LocalPlayer.Backpack["[Phone]"].Parent = game.Players.LocalPlayer.Character
+        game.ReplicatedStorage.MainEvent:FireServer("PhoneCall", tostring(v.Name))
+        task.wait()
+        game.Players.LocalPlayer.Character["[Phone]"].Parent = game.Players.LocalPlayer.Backpack
+        task.wait()
+        local success, result = pcall(StarterGui.SetCoreGuiEnabled, StarterGui, Enum.CoreGuiType.Backpack, true)
+        task.wait(.2)
+    end
+end
